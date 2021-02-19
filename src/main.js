d46dc4622e1dffcc1d844ceb97e80578a11494e8
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-19 10:18:38 +0800

    运行时版本，使用render操作DOM

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ Vue.config.productionTip = false
 // new Vue({
 //   render: h => h(App),
 // }).$mount('#app')
-
+console.log(Vue)
 new Vue({
   el: "#app",
+  render(h) {
+    return h("div", this.n)
+  },
   data() {
     return {
       n: 0
