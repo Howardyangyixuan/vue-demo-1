60c418aa5c41a93b0da68e9719cdce1d88d2fb4d
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-20 19:41:43 +0800

    使用template

--- src/main.js
+++ src/main.js
@@ -5,9 +5,24 @@ Vue.config.productionTip = false
 
 // }).$mount('#app')
 console.log(Demo.render.toString())
-new Vue({
+const vm = new Vue({
   // el: "#app",
-  render(h) {
-    return h(Demo)
+  // render(h) {
+  //   return h(Demo)
+  // }
+  template: `
+    <div>
+      {{n}}
+      <button @click="add">+1</button>
+    </div>
+  `,
+  data: {
+    n: 0
+  },
+  methods: {
+    add() {
+      this.n += 1
+    }
   }
-}).$mount("#app")
+})
+vm.$mount("#app")
