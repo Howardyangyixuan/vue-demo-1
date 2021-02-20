2eafb161fa5cb29d07efd4149f38a0ccdb3915c8
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-20 11:10:15 +0800

    运行时版本用h函数，实现+1按钮

--- src/main.js
+++ src/main.js
@@ -10,11 +10,17 @@ console.log(Vue)
 new Vue({
   el: "#app",
   render(h) {
-    return h("div", this.n)
+    return h("div", [this.n, h("button", {on: {click: this.add}}, "+1")])
   },
   data() {
     return {
       n: 0
+    }
+  },
+  methods: {
+    add() {
+      console.log("hi")
+      this.n += 1
     }
   }
 })
