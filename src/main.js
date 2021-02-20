3659695b9ecbbd256e00ecc279eee467a7f581e7
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-20 19:21:30 +0800

    通过两种方式挂载

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
-// import Vue from 'vue'
-import App from "./App.vue"
 import Demo from "./components/Demo"
 
 Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App),
 // }).$mount('#app')
 console.log(Demo.render.toString())
 new Vue({
-  el: "#app",
+  // el: "#app",
   render(h) {
     return h(Demo)
   }
-})
+}).$mount("#app")
