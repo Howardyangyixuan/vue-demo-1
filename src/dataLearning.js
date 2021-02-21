1762e80f364c97891667e92e4c6d0f6eab403f7c
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-21 11:14:48 +0800

    使用obj.defineProperty增加get、set

--- src/dataLearning.js
+++ src/dataLearning.js
@@ -40,11 +40,30 @@ let test = {
     this.姓 = name[0]
     this.名 = name.substring(1)
   },
-  changeName(name){
+  changeName(name) {
     this.姓名 = name
-    console.log("改完了，名叫:",this.姓名)
+    console.log("改完了，名叫:", this.姓名)
   }
 }
 
 test.changeName("Hjx")
 console.log("使用set,计算属性:" + test.姓名)
+
+Object.defineProperty(test, "全大写", {
+  get() {
+    return this.姓名.toUpperCase()
+  }
+})
+console.log("使用Object.defineProperty,get全大写计算属性:" + test.全大写)
+
+let age = 23
+Object.defineProperty(test, "年龄", {
+  get() {
+    return age
+  },
+  set(newAge) {
+    age = newAge
+  }
+})
+test.年龄 = 24
+console.log("使用Object.defineProperty,外部存储年龄计算属性:" + test.年龄)
