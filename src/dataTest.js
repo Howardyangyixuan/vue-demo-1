6fea89ae484f5f466f99d1cf7779242c46e26a98
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-22 11:10:29 +0800

    通过代理方式控制数据，监听方式控制数据源的改变，结合实现数据相应式

--- src/dataTest.js
+++ src/dataTest.js
@@ -20,11 +20,79 @@ Object.defineProperty(data1, "n", {
     n = v
   }
 })
-data1.n = 4
-n = 5
-data1.n = -3
+data1.n = 0
+n = 1
+data1.n = -1
 console.log("+data1:", data1)
 console.log("+data1.n:", data1.n)
-n = -3
+n = -1
 console.log("-data1:", data1)
 console.log("-data1.n:", data1.n)
+
+//使用匿名的n，使得无法修改
+let myData = {
+  n: 0
+}
+let data2 = proxy({data: myData})
+
+function proxy({data}) {
+  const obj = {}
+  Object.defineProperty(obj, "n", {
+    get() {
+      return data.n
+    },
+    set(v) {
+      if (v <= 0) return
+      data.n = v
+    },
+  })
+  return obj
+}
+
+data2.n = 2
+console.log("+data2:", data2)
+console.log("+data2.n:", data2.n)
+data2.n = -2
+console.log("-data2:", data2)
+console.log("-data2.n:", data2.n)
+myData.n = -2
+console.log("-data2:", data2)
+console.log("-data2.n:", data2.n)
+
+//使用匿名的n，使得无法修改
+let data3 = proxy2({data: myData})
+
+function proxy2({data}) {
+  let value = data.n
+  delete data.n
+  Object.defineProperty(data, "n", {
+    get() {
+      return value
+    },
+    set(v) {
+      if (v <= 0) return
+      value = v
+    },
+  })
+  const obj = {}
+  Object.defineProperty(obj, "n", {
+    get() {
+      return data.n
+    },
+    set(v) {
+      if (v <= 0) return
+      value = v
+    },
+  })
+  return obj
+}
+
+data3.n = 3
+console.log("+data3:", data3)
+console.log("+data3.n:", data3.n)
+data3.n = -3
+console.log("-data3:", data3)
+console.log("-data3.n:", data3.n)
+myData.n = -3
+console.log("-data3:", data3)
+console.log("-data3.n:", data3.n)
