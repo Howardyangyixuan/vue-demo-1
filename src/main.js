19e9693d99d725b92f02d22ef5b5d1d71b4b0995
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-01 10:33:55 +0800

    computed计算属性

--- src/main.js
+++ src/main.js
@@ -6,54 +6,40 @@ const Vue = window.Vue
 Vue.config.productionTip = false
 
 let myData = {
-  undeclared: {
-    a: 0
-  },
-  array: ["a", "b", "c"],
-  n: 0,
-  visible: true
+  word: "HI",
 }
 
 const vm = new Vue({
   template: `
     <div>
-      <button @click="set">set undeclared b</button>
-      <button @click="add">b+1</button>
-      <button @click="setPush">use set to push d to array</button>
-      <button @click="push">push e to array</button>
-      <button @click="change">add array 3</button>
+      <button @click="set">set</button>
+      word:{{word}}
       <hr>
-      main data:{{n}}
-      undeclared data:{{undeclared.b}}
-      array:{{array}}
+      logo word:{{logo}}
+      <hr>
+      lowercase word:{{getLower}}
     </div>
   `,
   data: myData,
+  computed: {
+    //computed这里必须有this
+    getLower() {
+      return this.word.toLowerCase()
+    },
+    logo: {
+      get() {
+        return this.word + "_yyx"
+      },
+      set(value) {
+        this.word += value
+      }
+    }
+  },
   methods: {
     set() {
-      //但是需要保证set是在其他赋值之前，否则会提前生成，如果已有数据则不会添加代理和监听
-      Vue.set(this.undeclared, "b", 1)
-      // this.$set(this.undeclared, "b", 1)
+      this.logo = "*"
     },
-    add() {
-      this.undeclared.b += 1
-      console.log(myData)
-    },
-    push() {
-      this.array.push("e")
-    },
-    setPush() {
-      Vue.set(this.array, "3", "d")
-    },
-    change() {
-      this.array[3] = 1
-    }
   }
 })
 vm.$mount("#app")
 
-setTimeout(() => {
-  //直接修改数据，即被监听对象，会触发全局更新
-  myData.n += 10
-  console.log(myData)
-}, 3000)
