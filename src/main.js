6ad305d6303d1c859ec60a3a28372c482d1755ab
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-04 20:38:31 +0800

    使用v-model处理 选框+输入

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Demo from "./components/Demo"
+// import Demo from "./components/Demo"
 // import "./dataLearning"
 // import "./dataTest"
 
@@ -6,39 +6,60 @@ const Vue = window.Vue
 Vue.config.productionTip = false
 
 let myData = {
-  word: "HI",
+  single: "A",
+  multiple: [],
+  line: "",
+  multiLine: [],
+  select: "",
+  multiSelect: [],
+  database: [
+    {id: 1, value: "to be"},
+    {id: 2, value: "not to be"},
+    {id: 3, value: "no answer"}
+  ]
 }
 
 const vm = new Vue({
   template: `
     <div>
-      <Demo/>
-      <button @click="set">set</button>
-      word:{{word}}
+      单选框:{{single}}
+      <label>
+        <input type="radio" v-model=single value="A">
+        <input type="radio" v-model=single value="B">
+      </label>
       <hr>
-      logo word:{{logo}}
+      复选框:{{multiple}}
+      <label>
+        <input type="checkbox" v-model=multiple value="a">
+      </label>
+      <label>
+        <input type="checkbox" v-model=multiple value="b">
+      </label>
+      <label>
+        <input type="checkbox" v-model=multiple value="c">
+      </label>
       <hr>
-      lowercase word:{{getLower}}
+      单行输入:{{line}}
+      <label>
+        <input type="text" v-model=line placeholder="fill in line">
+      </label>
+      <hr>
+      多行输入:{{multiLine}}
+      <textarea v-model=multiLine placeholder="fill in multiline"/>
+      <hr>
+      选择:{{select}}
+      <select v-model="select">
+        <option disabled value="">请选择</option>
+        <option v-for="item in database" :key="item.id">{{item.value}}</option>
+      </select>
+      <hr>
+      多选择(用的少，一般用ant-design封装):{{multiSelect}}
+      <select multiple v-model="multiSelect">
+        <option v-for="item in database" :key="item.id">{{item.value}}</option>
+      </select>
     </div>
   `,
-  components: {
-    Demo
-  },
   data: myData,
-  computed: {
-    //computed这里必须有this
-    getLower() {
-      return this.word.toLowerCase()
-    },
-    logo: {
-      get() {
-        return this.word + "_yyx"
-      },
-      set(value) {
-        this.word += value
-      }
-    }
-  },
   methods: {
     set() {
       this.logo = "*"
